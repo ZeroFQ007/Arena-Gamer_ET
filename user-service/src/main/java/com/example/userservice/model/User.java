1f9bacd89046583073eb28f0d11870d3a105ef7f
Your Name <tom.recabarren@duocuc.cl>
2026-05-16 00:47:00 -0400

    Fix: Corrección definitiva del monorepo, pom.xml limpio y microservicios conectados

--- user-service/src/main/java/com/example/userservice/model/User.java
+++ user-service/src/main/java/com/example/userservice/model/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-import javax.management.relation.Role;
 
 @Entity
 @Table(name ="users")
@@ -39,7 +38,8 @@ public class User {
         this.email = email;
         this.role = role;
     }
-    public Long getId() {{return id;}}
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
     public String getUsername() {return username;}
     public void setUsername(String username) {this.username = username;}
     public String getEmail() {return email;}
