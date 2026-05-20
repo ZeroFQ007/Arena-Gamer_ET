225e6764cf92b7ed838bc6754f11c43e19dae071
Fabrizio Quintini <fa.quintini@duocuc.cl>
2026-05-20 00:49:57 -0400

    fix: correcciones user-service y station-service

--- user-service/src/main/java/com/example/userservice/model/User.java
+++ user-service/src/main/java/com/example/userservice/model/User.java
@@ -1,5 +1,6 @@
 package com.example.userservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -25,6 +26,8 @@ public class User {
     @Column(nullable = false)
     private Role role;
 
+    @JsonIgnore
+    @Column(length = 255)
     private String password;
 
     @Column (nullable = false)
